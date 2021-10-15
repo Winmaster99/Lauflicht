8835d743ce8f558b80a9d8fad9ed05254968b269
Winmaster99 <Da.Winner@gmx.de>
2021-10-15 12:58:29 +0200

    Dritte Version

--- Lauflicht.py
+++ Lauflicht.py
@@ -5,50 +5,17 @@
 
 from machine import Pin
 import time
+#Pin_Liste
 
+leds = [2,27,26,25,33,32,13,17,12,15]
+for i in range(0,10):
+    leds[i] = Pin(leds[i],Pin.OUT)
 
-#Pinbelegung
-led1 = Pin(2,Pin.OUT)
-led2 = Pin(27,Pin.OUT)
-led3 = Pin(26,Pin.OUT)
-led4 = Pin(25,Pin.OUT)
-led5 = Pin(33,Pin.OUT)
-led6 = Pin(32,Pin.OUT)
-led7 = Pin(13,Pin.OUT)
-led8 = Pin(17,Pin.OUT)
-led9 = Pin(12,Pin.OUT)
-led10 = Pin(15,Pin.OUT)
-
-#Lauflicht Schleife
 while True:
-    time.sleep(0.1)
-    led1.value(1)
-    time.sleep(0.1)
-    led2.value(1)
-    time.sleep(0.1)
-    led3.value(1)
-    time.sleep(0.1)
-    led4.value(1)
-    time.sleep(0.1)
-    led5.value(1)
-    time.sleep(0.1)
-    led6.value(1)
-    time.sleep(0.1)
-    led7.value(1)
-    time.sleep(0.1)
-    led8.value(1)
-    time.sleep(0.1)
-    led9.value(1)
-    time.sleep(0.1)
-    led10.value(1)
-    time.sleep(0.1)
-    led1.value(0)
-    led2.value(0)
-    led3.value(0)
-    led4.value(0)
-    led5.value(0)
-    led6.value(0)
-    led7.value(0)
-    led8.value(0)
-    led9.value(0)
-    led10.value(0)
+    for i in range(0,10):
+        leds[i].value(1)
+        time.sleep(0.01)
+    for i in range(9,-1,-1):
+        leds[i].value(0)
+        time.sleep(0.01) 
+
